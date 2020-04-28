4531ad976d67a56560dc72a687421ac2910b1e01
vasilnv <38373222+vasilnv@users.noreply.github.com>
2020-04-28 20:15:01 +0300

    First commit

---
+++ TestTasks/task15.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ $EUID -ne 0 ]
+then 
+	echo "This script should be executed by root"
+	exit 1
+fi
+
+#no home dirs
+cat /etc/passwd | grep -v /home
+
+#home directories that don't exist
+usersWithHomeDirs=$(cat /etc/passwd | grep /home)
+while read line
+do
+	user=$(echo "$line" | tr -s "  " " " | cut -d ':' -f 1,7)
+	currentDir=$(echo "$user" | cut -d ':' -f2)
+	if [ ! -d currentDir ] 
+	then
+		echo "$line"
+	fi
+done < <(echo "$usersWithHomeDirs")
