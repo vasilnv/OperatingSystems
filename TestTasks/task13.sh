dc1c840690ed760f103ccd48a7d23367dbbe880c
vasilnv <38373222+vasilnv@users.noreply.github.com>
2020-04-28 19:25:50 +0300

    First commit

---
+++ TestTasks/task13.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ ! -d $1 ]
+then
+	echo you should add a directory as parameter
+fi
+
+directory=$1
+
+files=$(ls -lR /home | grep ^l | tr -s "  " " " | cut -d " " -f 9,11)
+while read line
+do
+	symLinkFile=$(echo "$line" | cut -d " " -f2)
+	if [ -f "$symLinkFile" ]
+	then
+		echo $directory/$(echo "$line" | cut -d " " -f1)
+	fi
+done < <(echo "$files")
