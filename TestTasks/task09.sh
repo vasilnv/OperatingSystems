fea9703f3bfc6f0a56680293b257366c1b022534
vasilnv <38373222+vasilnv@users.noreply.github.com>
2020-06-18 14:43:20 +0300

    Update task09 to use last modification time

--- TestTasks/task09.sh
+++ TestTasks/task09.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find /home/grade -type f -printf "%Ar %i %n\n" | sort -n | awk -F ' ' '$4>=1{print $3}' | tail -n 1
+find /home/grade -type f -printf "%i %T@ %n\n" | sort -t ' ' -k2 -nr | awk -F ' ' '{if($3 > 1) {print $1}}' | uniq | head -n 1
