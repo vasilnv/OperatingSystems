3e9132ee44405135939492a7d03a21a4d73d6d53
vasilnv <38373222+vasilnv@users.noreply.github.com>
2020-06-18 15:22:56 +0300

    Update task11 to use last status change time

--- TestTasks/task11.sh
+++ TestTasks/task11.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find /home/SI -type d -printf "%p %As\n" | awk -F " " '$2 > 1551168000 && $2 < 1551176100 {print $1}'
+find /home/SI -type d -printf "%p %Cs\n" | awk -F ' ' '$2 > 1551168000 && $2 < 1551176100 {print $1}'
