137dd61c50fb6ad768db8fc383c707a5cecfb439
vasilnv <38373222+vasilnv@users.noreply.github.com>
2020-06-21 17:21:56 +0300

    Format code

--- ShellScripts/05-b-9500.sh
+++ ShellScripts/05-b-9500.sh
@@ -5,15 +5,13 @@ string=$2
 if [ $option == "-r" ]
 then 
 	echo -e "\033[0;31m $string"
-else if [ $option == "-g" ]
+elif [ $option == "-g" ]
 then
 	echo -e "\033[0;32m $string"
-else if [ $option == "-b" ]
+elif [ $option == "-b" ]
 then
 	echo -e "\033[0;34m $string"
 else 
 	echo invalid first parameter
-fi
-fi
 fi
 echo -e "\033[0m"
