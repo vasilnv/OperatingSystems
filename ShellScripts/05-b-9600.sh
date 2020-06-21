09bdbf44bb72bd1f283a552acfa2132ca4e805a9
vasilnv <38373222+vasilnv@users.noreply.github.com>
2020-06-21 18:05:23 +0300

    Update task 05-b-9600.sh to work with xz

--- ShellScripts/05-b-9600.sh
+++ ShellScripts/05-b-9600.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 checkRecursive="do not delete"
-
+echo "${BACKUP_DIR}"
 for i in "$@" 
 do
 	if [ "$i" == '-r' ]
@@ -9,21 +9,26 @@ do
 		checkRecursive="delete"
 		continue
 	fi
+	
 	name=$(echo $i | rev | cut -d '/' -f1 | rev)
+	
 	if [ -f "$i" ] 
 	then
 		time=$(date +"%Y-%m-%d-%H-%M-%S")
-		gzip -f "$i" > "${BACKUP_DIR}/${name}_${time}.gz"
+		xz "$i" > "${BACKUP_DIR}/${name}_${time}"
+		rm "$i".xz
 	elif [ -d "$i" ] 
 	then
 		numOfFiles=$(ls -l "$i"| wc -l)
 		time=$(date +"%Y-%m-%d-%H-%M-%S")
 		if [ $numOfFiles -eq 1 ] 
 		then
-			tar -czf "${BACKUP_DIR}/${name}_${time}.tgz" "$i"
+			tar -cvf "${BACKUP_DIR}/${name}_${time}.tar" "$i"
+			xz "${BACKUP_DIR}/${name}_${time}.tar"
 			rmdir "$i" 
 		else
-			tar -czf "${BACKUP_DIR}/${name}_${time}.tgz" "$i"
+			tar -cvf "${BACKUP_DIR}/${name}_${time}.tar" "$i"
+			xz "${BACKUP_DIR}/${name}_${time}.tar"
 			if [ "$checkRecursive" == "delete" ] 
 			then
 				rm -r "$i"
