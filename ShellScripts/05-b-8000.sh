c37c411cc51b25aa13cd333dfbf8688b13f3ce76
vasilnv <38373222+vasilnv@users.noreply.github.com>
2020-06-21 16:44:30 +0300

    Add sorting by vsz and update error handling

--- ShellScripts/05-b-8000.sh
+++ ShellScripts/05-b-8000.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 user=$1
 
-ps -eo euser -o rss -o vsz -o cmd | grep "$user" | tr -s "  " " " | awk -F ' ' '{printf ""$4" %.2f\n", $2/$3}'
+ps -eo euser -o rss -o vsz -o cmd | grep "$user" | tr -s " " |sort -nr -t" " -k3 | awk -F ' ' '{if($3 != 0) {printf "%s %.2f\n", $4, $2/$3}}'
+
