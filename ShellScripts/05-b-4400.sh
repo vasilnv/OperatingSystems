9bf8f0fdd270fe0228dcd1aeeccaff9276bffe84
vasilnv <38373222+vasilnv@users.noreply.github.com>
2020-06-21 13:55:13 +0300

    Update task to be working with less parameters and last modification time

--- ShellScripts/05-b-4400.sh
+++ ShellScripts/05-b-4400.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-dirExtract=$1
-dirTo=$2
-cp $(find $dirExtract -type f -amin -45) $dirTo
-exit 0
+dir=$1
+if [ $# -eq 2 ]
+then
+	cpDir=$2
+elif [ $# -eq 1 ]
+then
+	cpDir=$(date)
+else
+	echo wrong number of parameters
+fi
+mkdir "$cpDir"
+find $dir -type f -mmin -45 -exec cp {} "$cpDir" \;
