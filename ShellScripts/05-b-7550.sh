484e940640128266d658b1dd76a63f085aa668d4
vasilnv <38373222+vasilnv@users.noreply.github.com>
2020-06-21 16:12:43 +0300

    Update task to work correctly

--- ShellScripts/05-b-7550.sh
+++ ShellScripts/05-b-7550.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-user=$1
+processes=$(ps -e -o user -o tty -o pid | tr -s " " | grep "grade pts/1" | cut -d ' ' -f3)
+while read line 
+do
+	kill -15 $line
+done < <(echo "$processes")
 
-ps -ef | grep "$user" | tr -s "  " " " | awk -F ' ' '{kill $2}'
+#do not use kill -KILL or kill -9 because the process would not 
+	#1.clean up temp files
+	#2.shut down sock
+	#3) inform its children that it is going away
+	#4) reset its terminal characteristics
+		#and so on and so on.
+#Generally, send 15, and wait a second or two, and if that does not work, send 2, 
+#and if that does not work send 1. If that does not work, REMOVE THE BINARY because
+#the program is badly behaved!
