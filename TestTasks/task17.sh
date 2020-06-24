c6ee485fed90053b8fe5e7c43e156ff14f10be25
vasilnv <38373222+vasilnv@users.noreply.github.com>
2020-06-25 00:51:52 +0300

    Update to work with average time of processes

--- TestTasks/task17.sh
+++ TestTasks/task17.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-processes=$(ps -e -o user, -o pid, -o etimes | sort -k1) 
+processes=$(ps -e -o user, -o pid, -o times --no-headers | sort -k1) 
 
 users=$(echo "${processes}" | cut -d ' ' -f1 | uniq)
 user=$1
-userProcessCounter=$(echo "${processes}" | grep $user | wc -l)
+userProcessCounter=$(echo "${processes}" | grep "${user}" | wc -l)
 
-if [ -z "$userProcessCounter" ]; then
+if [ -z "${userProcessCounter}" ]; then
 	userProcessCounter=0
 fi
 
 
-echo "$users" | while read line
+while read line
 do
     processCounter=$(echo "$processes" | grep "$line" | wc -l)
 
     if [ "$processCounter" -gt "$userProcessCounter" ]; then
     	echo "$line"	
     fi
-done
+done < <(echo "${users}")
 
-AVERAGE=$(echo "$processes" | tr -s "  " " " |  awk -F ' ' '{cnt+=$3} END {printf cnt/2}')
+AVERAGE=$(echo "$processes" | tr -s "  " " " |  awk -F ' ' '{t+=$3;cnt++} END {printf "%d", t/cnt}')
 
 echo "$AVERAGE"
 
-userProcesses=$(echo "$processes" | grep $user)
-
+userProcesses=$(echo "$processes")
+timecheck=$(expr 2 "*" ${AVERAGE})
 while read line
 do
 	currProcessTime=$(echo "$line" | tr -s "  " " " | cut -d ' ' -f3)
-	if [ $currProcessTime -gt $AVERAGE ]
+	if [ ${currProcessTime} -gt ${AVERAGE} ]
 	then
 		echo gona kill $line
 	fi 
