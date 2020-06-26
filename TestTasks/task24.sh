6c7abfe299244a21914b80f9830da80c03721690
vasilnv <38373222+vasilnv@users.noreply.github.com>
2020-06-26 13:34:05 +0300

    Update task24

--- TestTasks/task24.sh
+++ TestTasks/task24.sh
@@ -1,34 +1,29 @@
 #!/bin/bash
 
-rss=$(ps -eo euser -o rss | grep ^root | tr -s "  " " "  | awk -F ' ' '{total+=$2} END {print total}')
+if [ $(id -u) -ne 1000 ]
+then
+	echo script should be executed by grade
+fi
+
+rss=$(ps -eo user -o rss | grep ^root | tr -s " "  | awk -F ' ' '{total+=$2} END {print total}')
 
 users=$(cat /etc/passwd | cut -d ':' -f1,6)
 
 while read line
 do
 	user=$(echo $line | cut -d ':' -f1)
-	echo this is my user: $user
 	dir=$(echo $line | cut -d ':' -f2)
 
-	if [ ! -d $dir ]
+	if [ ! -d $dir ] || [ "$(stat -c "%U" $dir)" != "$user" ] || [ "$(stat -c "%A" $dir | cut -c3)" != "w" ]
 	then
 		user_rss=$(ps -eo euser -o rss | grep ^${user} | tr -s "  " " " | awk -F ' ' 'BEGIN {total=0} {total+=$2} END {print total}')
-	elif [ "$(stat -c "%U" $dir)" != "$user" ]
-	then
-		user_rss=$(ps -eo euser -o rss | grep ^${user} | tr -s "  " " " | awk -F ' ' 'BEGIN {total=0} {total+=$2} END {print total}')
-	elif [ "$(stat -c "%A" $dir | cut -c3)" != "w" ] 
-	then
-		 user_rss=$(ps -eo euser -o rss | grep ^${user} | tr -s "  " " " | awk -F ' ' 'BEGIN {total=0} {total+=$2} END {print total}')
 	else
-		echo i continue
 		continue
 	fi
-	echo this is userRss: $user_rss
-	echo this is totalRss: $rss
+
 	if [ $user_rss -gt $rss ]
 	then 
-		echo kill by user
+		echo killall -u "${user}" -KILL
 	fi
 
 done < <(echo "$users")
-
