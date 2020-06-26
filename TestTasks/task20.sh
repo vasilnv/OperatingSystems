c5db93562c20962e65c1d440c2bdad008a62e53e
vasilnv <38373222+vasilnv@users.noreply.github.com>
2020-06-26 09:39:50 +0300

    Update task20 to work correctly

--- TestTasks/task20.sh
+++ TestTasks/task20.sh
@@ -1,17 +1,41 @@
 #!/bin/bash
+if [ ! -d $1 ]
+then
+	echo first argument should be dir
+fi
+
+if [ ! -d $2 ]
+then 	
+	echo second argment should be dir
+fi
 
 if [ $# -ne 3 ]
 then
-	echo "wrong number of parameters"
+	echo wrong number of parameters
 fi
 
-sourceFile=$1
-destination=$2
-stringComparator=$3
+SRC=$1
+DST=$2
+STR=$3
 
-filesFound=$(find $SRC -type f | grep $stringComparator)
+dstCheck=$(find ${DST} -type f | wc -l)
+if [ ${dstCheck} -ne 0 ]
+then
+	echo destination dir should be empty
+fi
+
+all=$(find ${SRC} -type f -printf "%p\n" | egrep "${STR}[^/]*$")
+
 while read line
 do
-	cp $line $destination
-done < <(echo $filesFound)
-
+	extr=$(echo ${line} | cut -d/ -f2)
+	echo ${extr}
+	dirs=$(dirname ${extr})
+	echo $dirs
+	if [ "${dirs}" != '.' ]
+	then
+		mkdir -p ${dirs}
+	fi
+	
+	mv ${SRC}/${extr} ${DST}/${extr}
+done < <(echo "${all}")
