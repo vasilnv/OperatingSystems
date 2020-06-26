fe1d526292601aee63f44e7a15c2c0c950d67a9e
vasilnv <38373222+vasilnv@users.noreply.github.com>
2020-06-26 22:17:03 +0300

    Update error handling

--- TestTasks/task26.sh
+++ TestTasks/task26.sh
@@ -2,29 +2,41 @@
 if [ $# -ne 2 ]
 then
 	echo "wrong number of parameters"
-fi
-file=$1
-dir=$2
-if [ ! -f "${file}" ]
-then
-	echo "first parameter should be file"
+	exit 1
 fi
 
-if [ ! -d "${dir}" ]
+file=$1
+dir=$2
+
+if [ ! -f "${file}" ] || [ ! -r "${file}" ]
 then
-	echo "second parameter should be directory"
+	echo "first parameter should be a readable file"
+	exit 1
+fi
+
+if [ ! -d "${dir}" ] || [ ! -r "${file}" ]
+then
+	echo "second parameter should be a readable directory"
+	exit 1
+fi
+
+dirok=$(find "${dir}" -type f | wc -l)
+if [ ${dirok} -ne 0 ]
+then
+	echo "dir should be empty"
+	exit 1
 fi
 
 dict="${dir}"dict.txt
 touch "${dict}"
 
-redact=$(cat "${file}" | cut -d ' ' -f 1,2 | cut -d : -f1  | sort | uniq)
-echo "$redact"
+redact=$(cat "${file}" | cut -d ' ' -f 1,2 | cut -d: -f1 | sort | uniq)
+
 count=1
 while read line
 do
 	echo "${line}"';'"${count}" >>"${dict}"
-	count=$((count+1))
+	count=$(expr ${count} "+" 1)
 done < <(echo "${redact}")
 
 
@@ -36,3 +48,4 @@ do
 	name=$(echo "${line}" | cut -d ';' -f1)
 	cat "${file}" | grep "${name}" >> "${dir}""${num}"".txt"
 done < <(cat "${dict}")
+
